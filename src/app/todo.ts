91617deed3f1c71d518ccacaf256cec23f67f299
iq 18 <ivan.quirke@mycit.ie>
2019-06-22 22:41:51 +0100

    added menu bar and footer closes #3

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,14 +1,18 @@
+import { CompileTemplateMetadata } from '@angular/compiler';
+
 export class Todo {
 
   id: number;
   text: string;
   complete: boolean;
   dueDate: Date;
+  done: string;
 
   constructor(id: number, text: string, complete: boolean, dueDate: Date) {
       this.id = id;
       this.text = text;
       this.complete = complete;
       this.dueDate = dueDate;
+      this.done = this.complete ? 'True' : 'False';
   }
 }
